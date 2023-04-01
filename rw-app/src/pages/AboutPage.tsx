a00c24a9b5fc8a013ef9de8b40d12be9bcd9fce5
Eric Renken <eric@mynoc.com>
2023-04-01 18:37:23 -0400

    Updated about page.

--- rw-app/src/pages/AboutPage.tsx
+++ rw-app/src/pages/AboutPage.tsx
@@ -9,27 +9,29 @@ class AboutPage extends React.Component {
             <>
                 <br />
                 <Container>
-                    <div>
+                    <p>
                         Data on this site is powered by a Davis Vantage Pro/2 weather station and a Davis AirLink, and is currently being
                         uploaded to <a href='https://www.weatherlink.com/embeddablePage/show/94b5cab2a1f34a9c889e09cf33b9b2b8/summary'>WeatherLink</a> using
                         a Davis <a href='https://www.davisinstruments.com/pages/weatherlink-live'>WeatherLink Live</a>.
-                    </div>
-                    <br />
-                    <div>
-                        I am in the process of converting this site to a ReactJS site using Azure Functions as the API.  There is a Azure Function
-                        &nbsp;<a href='https://github.com/erenken/redfieldweather/blob/main/api/src/RedfieldWeather/WeatherLink/WeatherLinkGetCurrent.cs'>WeatherLinkGetCurrent</a> that pulls
-                        data from the <a href='https://weatherlink.github.io/v2-api/'>WeatherLink API</a> every five minutes and stores it to Azure Table Storage.  I am using this storage for current weather and history.
-                        This function uses a NuGet package <a href='https://www.nuget.org/packages/myNOC.WeatherLink'>myNOC.WeatherLink</a> I created to communicate with the WeatherLink API.
-                    </div>
-                    <br />
-                    <div>
+                    </p>
+                    <p>
+                        I am in the process of converting this site to a ReactJS site using Azure Functions as the API.  There are two a Azure Functions
+                        <ul>
+                            <li><a href='https://github.com/erenken/redfieldweather/blob/main/api/src/RedfieldWeather/WeatherLink/GetCurrent.cs'>GetCurrent</a></li>
+                            <li><a href='https://github.com/erenken/redfieldweather/blob/main/api/src/RedfieldWeather/WeatherLink/GetTodayHighLow.cs'>GetTodayHighLow</a></li>
+                        </ul>
+                        These function pull data from the <a href='https://weatherlink.github.io/v2-api/'>WeatherLink API</a> every five minutes and stores it to Azure Table Storage.  
+                        I am using this storage for current weather, highs &amp; lows, and history. This function uses a NuGet package <a href='https://www.nuget.org/packages/myNOC.WeatherLink'>myNOC.WeatherLink</a> 
+                        I created to communicate with the WeatherLink API.
+                    </p>
+                    <p>
                         You can view the code to this website on GitHub in the <a href='https://github.com/erenken/redfieldweather'>redfieldweather Repository</a>.
-                    </div>
-                    <div>
+                    </p>
+                    <p>
                         On my <a href='https://github.com/erenken'>GitHub</a> you can also find the code to my myNOC.WeatherLink NuGet package as well as other projects.
                         One of my projects is an <a href='https://github.com/erenken/gateMonitor/blob/main/projects/remootio-angular/README.md'>Angular Service</a> that
                         works with a Remootio smart gate conroller.
-                    </div>
+                    </p>
                     <hr />
                 </Container>
             </>
